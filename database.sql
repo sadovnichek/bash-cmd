b72c7c569cfc3851ebf2f93a43345bfde07dd8e4
Victor Kapkaev <78375906+sadovnichek@users.noreply.github.com>
2022-10-15 18:10:00 +0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,9 +6,9 @@ GO --РАЗДЕЛИТЕЛЬ БАТЧЕЙ (BATH)
 IF  EXISTS (
 	SELECT name 
 		FROM sys.databases 
-		WHERE name = N'KN304_KAPKAEV'
+		WHERE name = N'KN304_Kapkaev'
 )
-ALTER DATABASE [KN304_KAPKAEV] set single_user with rollback immediate
+ALTER DATABASE [KN304_Kapkaev] set single_user with rollback immediate
 GO
 /* ПРОВЕРЯЕМ, СУЩЕСТВУЕТ ЛИ НА СЕРВЕРЕ БАЗА ДАННЫХ
 С ИМЕНЕМ [ИМЯ БАЗЫ], ЕСЛИ ДА, ТО ЗАКРЫВАЕМ ВСЕ ТЕКУЩИЕ
@@ -17,9 +17,9 @@ GO
 IF  EXISTS (
 	SELECT name 
 		FROM sys.databases 
-		WHERE name = N'KN304_KAPKAEV'
+		WHERE name = N'KN304_Kapkaev'
 )
-DROP DATABASE [KN304_KAPKAEV]
+DROP DATABASE [KN304_Kapkaev]
 GO
 /* СНОВА ПРОВЕРЯЕМ, СУЩЕСТВУЕТ ЛИ НА СЕРВЕРЕ БАЗА ДАННЫХ
 С ИМЕНЕМ [ИМЯ БАЗЫ], ЕСЛИ ДА, УДАЛЯЕМ ЕЕ С СЕРВЕРА */
@@ -27,11 +27,11 @@ GO
 /* ДАННЫЙ БЛОК НЕОБХОДИМ ДЛЯ КОРРЕКТНОГО ПЕРЕСОЗДАНИЯ БАЗЫ
 ДАННЫХ С ИМЕНЕМ [ИМЯ БАЗЫ] ПРИ НЕОБХОДИМОСТИ */
 
-CREATE DATABASE [KN304_KAPKAEV]
+CREATE DATABASE [KN304_Kapkaev]
 GO
 -- СОЗДАЕМ БАЗУ ДАННЫХ
 
-USE [KN304_KAPKAEV]
+USE [KN304_Kapkaev]
 GO
 /* ПЕРЕХОДИМ К СОЗДАННОЙ БАЗЕ ДАННЫХ ДЛЯ ПОСЛЕДУЮЩЕЙ РАБОТЫ С НЕЙ 
 ИЛИ С ЭТИХ КОМАНД ПРОДОЛЖАЕМ РАБОТУ С БАЗОЙ ДАННЫХ ЕСЛИ ОНА
@@ -54,8 +54,8 @@ GO
 /*СОЗДАЕМ В БАЗЕ ДАННЫХ
  [ИМЯ БАЗЫ] СХЕМУ С ИМЕНЕМ Фамилия */
 
-IF OBJECT_ID('[KN304_KAPKAEV].Kapkaev.ut_students', 'U') IS NOT NULL
-  DROP TABLE  [KN304_KAPKAEV].Kapkaev.ut_students
+IF OBJECT_ID('[KN304_Kapkaev].Kapkaev.Shop', 'U') IS NOT NULL
+  DROP TABLE  [KN304_Kapkaev].Kapkaev.Shop
 GO
 
 /*ПРОВЕРЯЕМ, СУЩЕСТВУЕТ ЛИ В БАЗЕ ДАННЫХ
@@ -101,18 +101,18 @@ GO
 
 /*УДАЛЕНИЕ ОБЪЕКТА ТАБЛИЦЫ ИЗ БАЗЫ ДАННЫХ  */
 
-CREATE TABLE [KN304_KAPKAEV].Kapkaev.ItemGroup
+CREATE TABLE [KN304_Kapkaev].Kapkaev.ItemGroup
 (
 	group_id smallint Not NULL,
 	group_name varchar(50) NULL
+
 	CONSTRAINT PK_group_id PRIMARY KEY (group_id) 
 )
 GO
 
-CREATE TABLE [KN304_KAPKAEV].Kapkaev.Items
+CREATE TABLE [KN304_Kapkaev].Kapkaev.Items
 (
 	item_id int Not NULL,
-	group_name varchar(50) NULL,
 	group_id smallint Not NULL,
 	item_name varchar(50) NULL,
 	unit varchar(10) NULL,
@@ -124,7 +124,7 @@ CREATE TABLE [KN304_KAPKAEV].Kapkaev.Items
 )
 GO
 
-CREATE TABLE [KN304_KAPKAEV].Kapkaev.Storage
+CREATE TABLE [KN304_Kapkaev].Kapkaev.Storage
 (
 	item_id int Not NULL,
 	shop_id tinyint Not NULL,
@@ -179,12 +179,14 @@ SELECT * From [ИМЯ БАЗЫ].Фамилия.ut_nameGroup
 --ПРОСМАТРИВАЕМ СОДЕРЖИМОЕ ТАБЛИЦЫ ut_nameGroup
 
 
-INSERT INTO [KN304_KAPKAEV].Kapkaev.Shop 
+INSERT INTO [KN304_Kapkaev].Kapkaev.Shop 
   VALUES 
 	(1,N'Monetka', N'Flower','35'),
 	(2,N'Magnit', N'Stroiteley','55'),
 	(3,N'Kirovskiy', N'Kosmonavtov','80'),
 	(4,N'Pyaterochka', N'Morskaya','8')
+
+SELECT * From [KN304_Kapkaev].Kapkaev.Shop
 
 INSERT INTO [KN304_KAPKAEV].Kapkaev.ItemGroup 
   VALUES 
@@ -193,12 +195,50 @@ INSERT INTO [KN304_KAPKAEV].Kapkaev.ItemGroup
 	(353,N'Milks'),
 	(5,N'Meat')
 
-INSERT INTO [KN304_KAPKAEV].Kapkaev.Items
+SELECT * From [KN304_Kapkaev].Kapkaev.ItemGroup
+
+INSERT INTO [KN304_Kapkaev].Kapkaev.Items
   VALUES 
-	(89,N'Fruits'),
-	(55,N'Vegetables'),
-	(353,N'Milks'),
-	(5,N'Meat')
+	(89, 880, N'Apples', N'kg'),
+	(82, 880, N'Bananas', N'kg'),
+	(63, 55, N'Carrot', N'kg'),
+	(5, 55, N'Potatos', N'kg'),
+	(68, 55, N'Tomato', N'kg'),
+	(189, 353, N'Milk', N'l'),
+	(282, 353, N'Quark', N'kg'),
+	(363, 353, N'Yougurt', N'l'),
+	(405, 5, N'Pork', N'kg'),
+	(568, 5, N'Chiken', N'kg')
+
+
+INSERT INTO [KN304_Kapkaev].Kapkaev.Storage
+  VALUES 
+	(89, 1, '15-10-22 12:32', 80, 105, 0),
+	(89, 2, '14-10-22 13:48', 75, 100, 1),
+	(89, 3, '15-10-22 12:00', 90, 200, 5),
+
+	(82, 1, '11-10-22 11:32', 50, 52, 3),
+	(82, 3, '15-10-22 15:33', 48, 58, 10),
+	(82, 4, '13-10-22 11:00', 39, 55, 5),
+
+	(63, 2, '10-10-22 11:32', 60, 22, 3),
+	(63, 3, '14-10-22 15:33', 70, 40, 1),
+
+	(5, 1, '10-10-22 11:32', 25, 50, 3),
+	(5, 2, '14-10-22 15:33', 22, 60, 1),
+	(5, 4, '15-10-22 11:00', 30, 70, 8),
+
+	(189, 1, '10-10-22 11:30', 60, 250, 13),
+	(189, 2, '14-10-22 15:28', 70, 160, 11),
+	(189, 3, '15-10-22 11:24', 65, 170, 28),
+	(189, 4, '15-10-22 11:48', 63, 170, 18),
+
+	(363, 1, '15-10-22 11:53', 28, 70, 0),
+	(363, 4, '15-10-22 11:05', 30, 25, 8),
+
+	(568, 1, '14-10-22 15:06', 220, 600, 10),
+	(568, 3, '15-10-22 11:04', 300, 700, 80),
+	(568, 4, '15-10-22 11:30', 307, 705, 84)
 
  /*ВНОСИМ ДАННЫЕ В ТАБЛИЦУ ut_students, 
  ПОСКОЛЬКУ ЗАПОЛНЯЮТСЯ ВСЕ ПОЛЯ, ПИШУТЬСЯ ТОЛЬКО ДАННЫЕ
@@ -222,4 +262,3 @@ SELECT * From [ИМЯ БАЗЫ].Фамилия.ut_nameGroup
 
 SELECT * From [ИМЯ БАЗЫ].Фамилия.ut_students
 --ПРОСМАТРИВАЕМ СОДЕРЖИМОЕ ТАБЛИЦЫ ut_students   
-
